08c2deada89570899a765a1ff1299c5105dd8af8
ArkGame84 <Mahdi.khari@gmail.com>
2016-09-06 22:51:47 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {13844831},
+    sudo_users = {113567705},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
